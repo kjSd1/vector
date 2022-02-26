152bdae67021c78941589a16f77f97949d62002f
kjSd1 <cska_yfr@mail.ru>
2022-02-26 21:01:18 +0300

    init vectorVoid

---
+++ libs/data_structures/vectorVoid/vectorVoid.h
@@ -0,0 +1,17 @@
+#ifndef VECTOR_VECTORVOID_H
+#define VECTOR_VECTORVOID_H
+
+#include <limits.h>
+
+typedef struct vectorVoid {
+    void *data; // указатель на нулевой элемент вектора
+    size_t size; // размер вектора
+    size_t capacity; // вместимость вектора
+    size_t baseTypeSize; // размер базового типа:
+    // например, если вектор хранит int -
+    // то поле baseTypeSize = sizeof(int)
+    // если вектор хранит float -
+    // то поле baseTypeSize = sizeof(float)
+}vectorVoid;
+
+#endif //VECTOR_VECTORVOID_H

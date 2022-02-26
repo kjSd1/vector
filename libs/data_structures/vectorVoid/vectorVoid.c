4f9c5199fd070762160ae31dff189f7c5b4ffea2
kjSd1 <cska_yfr@mail.ru>
2022-02-26 21:13:06 +0300

    memory usage of vectorVoid

--- libs/data_structures/vectorVoid/vectorVoid.c
+++ libs/data_structures/vectorVoid/vectorVoid.c
@@ -1,1 +1,40 @@
 #include "vectorVoid.h"
+
+vectorVoid createVectorV(size_t n, size_t baseTypeSize) {
+    int *data = calloc(n, baseTypeSize);
+    if (data == NULL) {
+        fprintf(stderr, "bad alloc");
+        exit(1);
+    }
+    return (vectorVoid) {data, 0, n, baseTypeSize};
+}
+
+void reserve(vectorVoid *v, size_t newCapacity) {
+    if (newCapacity == 0)
+        v->data = NULL;
+    else if (v->capacity != newCapacity) {
+        v->data = realloc(v->data, newCapacity * v->baseTypeSize);
+        if (v->data == NULL) {
+            fprintf(stderr, "bad alloc");
+            exit(1);
+        }
+    }
+    v->capacity = newCapacity;
+    if (v->size > v->capacity)
+        v->size = v->capacity;
+}
+
+void clear(vectorVoid *v) {
+    v->size = 0;
+}
+
+void shrinkToFit(vectorVoid *v) {
+    reserve(v, v->size);
+}
+
+void deleteVector(vectorVoid *v) {
+    free(v->data);
+    v->data = NULL;
+    v->size = 0;
+    v->capacity = 0;
+}
--- libs/data_structures/vectorVoid/vectorVoid.h
+++ libs/data_structures/vectorVoid/vectorVoid.h
@@ -2,16 +2,35 @@
 #define VECTOR_VECTORVOID_H
 
 #include <limits.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
 
 typedef struct vectorVoid {
-    void *data; // указатель на нулевой элемент вектора
-    size_t size; // размер вектора
-    size_t capacity; // вместимость вектора
+    void *data;          // указатель на нулевой элемент вектора
+    size_t size;         // размер вектора
+    size_t capacity;     // вместимость вектора
     size_t baseTypeSize; // размер базового типа:
     // например, если вектор хранит int -
     // то поле baseTypeSize = sizeof(int)
     // если вектор хранит float -
     // то поле baseTypeSize = sizeof(float)
 }vectorVoid;
+
+// Возвращает структуру vectorVoid c вместимостью n элементов и базовым размером baseTypeSize
+vectorVoid createVectorV(size_t n, size_t baseTypeSize);
+
+// зменяет вместимость вектора v на newCapacity элементов
+void reserveV(vectorVoid *v, size_t newCapacity);
+
+// Очистка элементов, без освобождения памяти вектора v
+void clearV(vectorVoid *v);
+
+// Освобождает неиспользуемую память вектора v
+void shrinkToFitV(vectorVoid *v);
+
+// Освобождает память, выделенную вектор
+void deleteVectorV(vectorVoid *v);
 
 #endif //VECTOR_VECTORVOID_H
